b01ae977e3afd9fa9d73debe019032cfd63a03a9
Priyankapeaceie <161949968+Priyankapeaceie@users.noreply.github.com>
2025-12-17 09:58:33 +0530

    MY loop program

---
+++ Java_Code/src/com/tnsif/basics/ForLoopExample.java
@@ -0,0 +1,12 @@
+package com.tnsif.basics;
+
+public class ForLoopExample {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=1000;i--) {
+			System.out.println("Value of i:");
+			System.out.println(i);
+		}
+	}
+
+}

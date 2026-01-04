512060606c50d294c6088ff00d707dc9353535d9
priyankazen11-jpg <priyankazen11@gmail.com>
2026-01-04 22:33:42 +0530

    Delete SmartBankingSystem directory

--- SmartBankingSystem/src/com/bank/exception/InsufficientBalanceException.java
+++
@@ -1,9 +0,0 @@
-package com.bank.exception;
-
-public class InsufficientBalanceException extends Exception{
-	 public InsufficientBalanceException(String message) {
-	        super(message);
-	    }
-
-	 
-}
